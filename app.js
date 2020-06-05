86c9cdc336458ac696ca8c15d231ccc5688b43af
Fraser R <frasbr@gmail.com>
2020-06-05 17:31:53 +1000

    added API request and response validation

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 const EG = require("./expressionGenerator");
+const axios = require("axios");
 
 const eg = new EG();
 
 const array = [];
 
-array.push(eg.add(1, 4));
-array.push(eg.subtract(10, 4));
+array.push(eg.add("10 + 6", 4));
+array.push(eg.subtract(10, "2 + 2"));
 array.push(eg.multiply(7, 7));
 array.push(eg.divide(10, 2));
 array.push(eg.sqrt(100));
 
-console.log(array);
+const arrayToPost = array.map((expr) => expr.string);
+
+axios
+    .post(`http://api.mathjs.org/v4/`, {
+        expr: arrayToPost,
+    })
+    .then(({ data }) => {
+        array.forEach((expression, i) => {
+            console.log("input: ", expression.string);
+            console.log("output: ", data.result[i]);
+            console.log("expected: ", expression.result);
+            Number(data.result[i]) === expression.result
+                ? console.log("correct\n")
+                : console.log("incorrect\n");
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
